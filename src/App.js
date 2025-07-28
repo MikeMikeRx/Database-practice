d2c8accd3c27d5edc34e9dc1af80ead64ccc4ded
MichalRais <Michal.Rais@iCloud.com>
2025-07-28 13:46:05 +0100

    Add Form structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { useState, useEffect } from "react"
 const App = () => {
   const [data, setData] = useState([])
   const [error, setError] = useState(false)
+  //useState - form
+  const [movieTitle, setMovieTitle] = useState("")
+  const [movieAge, setMovieAge] = useState(null)
+  const [movieTime, setMovieTime] = useState(null)
 
   useEffect( ()=>{
     const unsubscribe = projectFirestore.collection("movies").onSnapshot( (snapshot)=>{
@@ -31,6 +35,14 @@ const App = () => {
 
 
   return <div className="all-movies">
+    <form>
+      <input type="text" placeholder="Movie name"/><br />
+      <input type="number" placeholder="Minimum Age" min="0" /><br />
+      <input type="number" placeholder="Movie duration" min="0"/><br />
+      <input type="submit" value="Add movie"/>
+
+    </form>
+
     {error && <p>{error}</p>}
     {data.map( (oneMovie)=> {
       const {id, title, minage, time} = oneMovie
