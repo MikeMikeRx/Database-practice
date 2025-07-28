76075ee323baeaef0006cd73919aae21477fbbdb
MichalRais <Michal.Rais@iCloud.com>
2025-07-28 10:34:59 +0100

    Connect to database and Add error message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { projectFirestore } from "./firebase/config"
+import { useState, useEffect } from "react"
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+
+  useEffect( ()=>{
+    projectFirestore.collection("movies").get().then( (snapshot)=>{
+      console.log(snapshot);
+
+      if(snapshot.empty){
+        setError("No files found")
+      }
+      
+    })
+  }, [])
+
+  return <div>
+    {error && <p>{error}</p>}
+  </div>
+  
 }
 
 export default App
